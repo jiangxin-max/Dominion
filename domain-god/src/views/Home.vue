e9cf778eb23c2bf0bb9d7fe033f4194578e3169a
jiangxin-max <1750912515@qq.com>
2020-11-08 19:24:51 +0800

    test commit

--- domain-god/src/views/Home.vue
+++ domain-god/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to my vue project"/>
+    <HelloWorld msg="Welcome to my vue project!"/>
   </div>
 </template>
 
